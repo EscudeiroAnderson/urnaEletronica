2475b490a4d1246e525c7ab8846ee718c48a0af5
Anderson <anderson.esc1@gmail.com>
2021-08-10 16:17:27 -0300

    urna eletrônica

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = 
+[
+    {
+            titulo:"VEREADOR",
+            numeros:5,
+            candidatos:
+        [
+            {
+
+                numero: '11111',
+                nome:"Gogo",
+                partido:"SBT",
+                fotos:[
+                    {url:'gogo.jpg', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero: '22222',
+                nome:"Plineo",
+                partido:"PLS",
+                fotos:[
+                    {url:'plineo.jpg', legenda:'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo:"PREFEITO",
+        numeros:2,
+        candidatos:[
+            {
+                numero:'11',
+                nome:"Silvio",
+                partido:'SBT',
+                Vice: 'Carlos',
+                fotos:[
+                {url:'silvio.jpg', legenda: 'Prefeito' },
+                {url:'c-a.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'22',
+                nome:"Tiago",
+                partido:'PLS',
+                Vice: 'Angélica',
+                fotos:[
+                {url:'tiago.jpg', legenda: 'Prefeito' },
+                {url:'angelica.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+
+    }
+];
